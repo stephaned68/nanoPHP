9d21130647f17a0ae8e006019155fb4d7b3054e2
Stéphane Devouard <sdevouard@gmail.com>
2020-07-23 10:54:20 +0200

    Added new GetAllWithCategory() which loads a list Contacts & Categories entities from a single SQL query

--- app/models/ContactRepository.php
+++ app/models/ContactRepository.php
@@ -4,7 +4,10 @@
 namespace app\models;
 
 
+use Exception;
 use framework\BaseRepository;
+use framework\Database;
+use framework\QueryBuilder;
 
 /**
  * DAO functions for the Contact entity
@@ -13,6 +16,40 @@ use framework\BaseRepository;
  */
 class ContactRepository extends BaseRepository
 {
+  /**
+   * @return array
+   * @throws Exception
+   */
+  public function getAllWithCategory()
+  {
+    $all = [];
+
+    try {
+      $qb = new QueryBuilder($this->table);
+      $qb
+        ->inner("categories","category_id", "category_id")
+        ->select();
+      var_dump($qb->getQuery());
+      $statement = Database::execute($qb->getQuery());
+      $qb = null;
+      while ($row = $statement->fetch()) {
+        $contact = new Contact();
+        Database::hydrate($contact, $row);
+        $category = new Category();
+        Database::hydrate($category, $row);
+        $contact->setCategory($category);
+        $all[] = $contact;
+        $category = null;
+        $contact = null;
+      }
+    } catch (Exception $e) {
+      throw new Exception($e->getMessage());
+    }
+
+    return $all;
+
+  }
+
   public function getOne($id): ?Contact
   {
     return parent::getOne($id);
