0290019ea9a8875994637bdecb00de0f4df108a5
Stéphane Devouard <sdevouard@gmail.com>
2020-07-24 19:05:05 +0200

    Added ucFirst() function to capitalize text and select2() function to enable select2 on all select elements with .select2 class

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -4,14 +4,26 @@
  * @returns {string}
  */
 function slugify(s) {
-  return s.toString().normalize('NFD').replace(/[\u0300-\u036f]/g, "") //remove diacritics
+  return s.toString()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, "") //   remove diacritics
     .toLowerCase()
-    .replace(/\s+/g, '-') //spaces to dashes
-    .replace(/&/g, '-and-') //ampersand to and
-    .replace(/[^\w\-]+/g, '') //remove non-words
-    .replace(/\-\-+/g, '-') //collapse multiple dashes
-    .replace(/^-+/, '') //trim starting dash
-    .replace(/-+$/, ''); //trim ending dash
+    .replace(/\s+/g, '-') //              spaces to dashes
+    .replace(/&/g, '-and-') //            ampersand to and
+    .replace(/[^\w\-]+/g, '') //          remove non-words
+    .replace(/\-\-+/g, '-') //            collapse multiple dashes
+    .replace(/^-+/, '') //                trim starting dash
+    .replace(/-+$/, '') //                trim ending dash
+    ;
+}
+
+/**
+ * Capitalize first letter
+ * @param text
+ * @returns {string|string}
+ */
+function ucFirst(text) {
+  return (text[0].toUpperCase() + text.slice(1)) || '';
 }
 
 /**
@@ -25,7 +37,7 @@ function cleanText(text) {
     text = text.replace(/\n/g, " ");
     text = text.replace(/'/g, "’");
     text = text.replace(/§/g, "\n");
-    text = text[0].toUpperCase() + text.slice(1);
+    text = ucFirst(text);
   }
   return text;
 }
@@ -57,4 +69,17 @@ function confirmDelete(formTitle) {
       }
     });
   });
+}
+
+/**
+ * Enable select2 override
+ * @param cssClass
+ */
+function select2(cssClass) {
+  cssClass = cssClass || 'select2';
+  if (!cssClass.startsWith('.')) cssClass = "." + cssClass;
+  const dropdowns = document.querySelectorAll('select' + cssClass);
+  for (const dropdown of dropdowns) {
+    $(dropdown).select2();
+  }
 }
